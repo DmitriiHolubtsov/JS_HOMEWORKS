0a35cbbc12f420fd1ec48ad0a051304be21bc50e
Dmitrii Holubtsov <dmitrii.h@ideasoft.io>
2024-02-22 22:04:08 +0200

    hw_5 - Hello user

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-### HOMEWORK #1
+### HOMEWORK #5
+
+# Hello user
--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 'use strict';
+
+// #5 Hello user
+
+let userName = prompt("Please, enter your name.");
+alert("Hello, " + userName + "! " + "How are you?");
