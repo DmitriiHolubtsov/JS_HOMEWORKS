1f8b67fc4b76ea53de8d50e972259f2b3fe2429d
Dmitrii Holubtsov <dmitrii.h@ideasoft.io>
2024-02-22 22:20:43 +0200

    hw_7 - 3 tasks

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-### HOMEWORK #1
+### HOMEWORK #7
+
+# 3 tasks
--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// #1
+let name;
+let admin;
+
+name = "John";
+admin = name;
+
+alert(admin);
+
+// #2
+let planetEarth;
+let user;
+
+// #3
+let sum = "10" + 10;
+console.log(typeof sum);
