0c03f72b1ec2b9ea1f47483bbbcda03870c25a44
Dmitrii Holubtsov <dmitrii.h@ideasoft.io>
2024-02-28 22:12:57 +0200

    hw_9 - Temperature Converter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// far => cel
+const getFahrenheitValue = prompt("Convert fahrenheit into celsius?");
+if (getFahrenheitValue) {
+    const calcCelsius = (getFahrenheitValue - 32) * 5/9;
+    alert(calcCelsius);
+}
+
+// cel => far
+const getCelsiusValue = prompt("Convert celsius into fahrenheit?");
+if (getCelsiusValue) {
+    const calcFahrenheit = (getCelsiusValue * 9/5) + 32;
+    alert(calcFahrenheit);
+}
