96b3493b8d6892835e51eae5c5e02961a7f0251f
Dmitrii Holubtsov <dmitrii.h@ideasoft.io>
2024-02-28 22:16:17 +0200

    hw_8 - Add const

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 'use strict';
 
-let userAge = +prompt("Please, enter your current age?");
+const userAge = +prompt("Please, enter your current age?");
 alert(`Your age, after 10 years will be ${userAge + 10} years.`);
