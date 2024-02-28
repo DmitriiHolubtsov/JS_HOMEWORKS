1cd354b40f217a424a7b84fecae8a1a632c234b2
Dmitrii Holubtsov <dmitrii.h@ideasoft.io>
2024-02-28 22:43:35 +0200

    hw_9 - Add variants

--- js/script.js
+++ js/script.js
@@ -1,15 +1,48 @@
 'use strict';
 
+// First variant
 // far => cel
 const getFahrenheitValue = prompt("Convert fahrenheit into celsius?");
 if (getFahrenheitValue) {
     const calcCelsius = (getFahrenheitValue - 32) * 5/9;
-    alert(calcCelsius);
+    alert(`${calcCelsius} C`);
 }
 
 // cel => far
 const getCelsiusValue = prompt("Convert celsius into fahrenheit?");
 if (getCelsiusValue) {
     const calcFahrenheit = (getCelsiusValue * 9/5) + 32;
-    alert(calcFahrenheit);
+    alert(`${calcFahrenheit} F`);
+}
+
+//Second variant
+const getValue = prompt("Please, enter degrees:");
+const getType = prompt("Please, enter 'C' - celsius or 'F' - fahrenheit ").toLowerCase();
+
+if (getType === "c") {
+    // far => cel
+    const calcCelsius = (getValue - 32) * 5/9;
+    alert(`${calcCelsius} C`);
+} else if (getType === "f") {
+    // cel => far
+    const calcFahrenheit = (+getValue * 9/5) + 32;
+    alert(`${calcFahrenheit} F`);
+}
+
+// Third variant
+const degreeInput = document.getElementById('degreeInput');
+const degreeType = document.getElementById('degreeType');
+const degreeResult = document.getElementById('degreeResult');
+
+const calculateDegree = () => {
+    if (degreeType.value === "C") {
+        //far > cel
+        const calcCel = (degreeInput.value - 32) * 5/9;
+        degreeResult.innerHTML = `<span>${calcCel} C</span>`;
+    }
+    else if (degreeType.value === "F") {
+        //cel > far
+        const calcFar = (degreeInput.value * 9/5) + 32;
+        degreeResult.innerHTML = `<span>${calcFar} F</span>`;
+    }
 }
