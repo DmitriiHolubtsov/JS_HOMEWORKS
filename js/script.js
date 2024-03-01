5a8b40340b6bcd526bf05cf5bbebb959c0d36826
Dmitrii Holubtsov <dmitrii.h@ideasoft.io>
2024-03-01 13:24:52 +0200

    hw_10 - Wired Calculator

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const firstOperand = +prompt("Please, enter first number to calculate?");
+const secondOperand = +prompt("Please, enter second number to calculate?");
+
+if(!isNaN(firstOperand && secondOperand)) {
+    if(firstOperand && secondOperand) {
+        console.log(`Result: ${firstOperand} + ${secondOperand} = ${firstOperand + secondOperand}`);
+        console.log(`Result: ${firstOperand} - ${secondOperand} = ${firstOperand - secondOperand}`);
+        console.log(`Result: ${firstOperand} * ${secondOperand} = ${firstOperand * secondOperand}`);
+        console.log(`Result: ${firstOperand} : ${secondOperand} = ${firstOperand / secondOperand}`);
+    } else {
+        alert("No value entered!")
+    }
+} else {
+    alert("You value is not a number!");
+}
