b96590049da2e7701ddde51df049e1535383c65f
Dmitrii Holubtsov <dmitrii.h@ideasoft.io>
2024-03-05 13:11:16 +0200

    hw_16 - Average value

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-### HOMEWORK #1
+### HOMEWORK #16
+
+# Average value
--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const firstNumber = +prompt("Enter first number?");
+const secondNumber = +prompt("Enter second number?");
+const thirdNumber = +prompt("Enter third number?");
+
+if(!isNaN(firstNumber && secondNumber && thirdNumber)) {
+    alert(`${(firstNumber + secondNumber + thirdNumber) / 3} `);
+} else (
+    alert("Enter valid value!")
+)
