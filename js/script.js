f0d94be80e80a9d37d7897e856a97e159bd5ae51
Dmitrii Holubtsov <dmitrii.h@ideasoft.io>
2024-03-05 20:34:24 +0200

    hw_15 - Calculator

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const operationType = prompt("Enter a type of operation: add, sub, mult, div?").toLowerCase();
+const firstValue = +prompt("Enter first number to calculate?");
+const secondValue = +prompt("Enter second number to calculate?");
+
+if(!isNaN(firstValue && secondValue) && operationType) {
+    if(operationType === 'add') {
+        alert(`Result: ${firstValue} + ${secondValue} = ${firstValue + secondValue}`);
+    } else if (operationType === 'sub') {
+        alert(`Result: ${firstValue} - ${secondValue} = ${firstValue - secondValue}`);
+    } else if (operationType === 'mult') {
+        alert(`Result: ${firstValue} * ${secondValue} = ${firstValue * secondValue}`);
+    } else if (operationType === 'div') {
+        alert(`Result: ${firstValue} : ${secondValue} = ${firstValue / secondValue}`);
+    } else {
+        alert("You have entered a wrong type of operation!");
+    }
+} else {
+    alert("Please check if all information is correct and try again.");
+}
