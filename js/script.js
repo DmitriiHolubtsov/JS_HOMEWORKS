2aa03371b4b716a677223111328eb2cf67a44df6
Dmitrii Holubtsov <dmitrii.h@ideasoft.io>
2024-03-05 13:32:30 +0200

    hw_14 - Mathematical operations

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const firstNumber = +prompt("Please enter first number?");
-const secondNumber = +prompt("Please enter second number? ");
+const secondNumber = +prompt("Please enter second number?");
 
 if(!isNaN(firstNumber && secondNumber)) {
         alert(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}\n${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}\n${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}\n${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}\n`);
