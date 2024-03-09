980a79c106ddd2c59bfe0044edede1c48f51f9cd
Dmitrii Holubtsov <dmitrii.h@ideasoft.io>
2024-03-09 14:08:44 +0200

    hw_18 - Convert if/else into switch/case statement

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-### HOMEWORK #1
+### HOMEWORK #18
+
+# convert if/else into switch statement
--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+let numOrStr = prompt('input number or string');
+console.log(numOrStr);
+
+// if/else
+
+if(numOrStr === null) {
+    console.log('ви скасували');
+} else if(numOrStr.trim() === '') {
+    console.log('Empty String');
+} else if (isNaN(+numOrStr)) {
+    console.log('number is Ba_NaN');
+} else {
+    console.log('OK!');
+}
+
+// switch
+
+switch (true) {
+    case (numOrStr === null):
+        console.log('ви скасували');
+        break;
+    case (numOrStr.trim() === ''):
+        console.log('Empty String');
+        break;
+    case (isNaN(+numOrStr)):
+        console.log('number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
+        break;
+}
