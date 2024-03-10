5d5232c70ad18a0b85e4df05116cbf42b897c5b8
Dmitrii Holubtsov <dmitrii.h@ideasoft.io>
2024-03-10 15:00:50 +0200

    hw_19 - Statements (Years)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const num = +prompt('Enter a number?');
+
+if(!isNaN(num)) {
+    if(num === 1) {
+        alert(`${num} рік`);
+    } else if(num > 1 && num < 5){
+        alert(`${num} роки`);
+    } else if(num >= 5 && num < 1000) {
+        alert(`${num} років`);
+    } else if(num >= 1000) {
+        alert(`${num} року`);
+    } else {
+        alert('Невірний вік.')
+    }
+} else {
+    alert('Ваше значення не вірне!');
+}
