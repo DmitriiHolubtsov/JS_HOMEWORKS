78995a2f447c4f737f42d9276c95ba8016f209f8
Dmitrii Holubtsov <dmitrii.h@ideasoft.io>
2024-03-13 23:48:04 +0200

    hw_25 - Labeled statement

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-### HOMEWORK #1
+### HOMEWORK #26
+
+# Labeled statement
--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+let sumOfNumbers = 0;
+loop: for(let i = 1; i <= 20; i++) {
+    if(i % 2 === 0) {
+        continue loop;
+    }
+    sumOfNumbers += i;
+}
+console.log('Result', sumOfNumbers);
