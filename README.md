e53b94cdc576ef0f4a230dd40c918f931ba076d3
Dmitrii Holubtsov <dmitrii.h@ideasoft.io>
2024-03-13 16:48:26 +0200

    hw_25 - Minimum divisor

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-### HOMEWORK #1
+### HOMEWORK #25
+
+# Minimum divisor
--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const num = parseInt(prompt('Enter your number?'));
+
+if(isNaN(num) || num < 1) {
+    console.log('NaN');
+} else {
+    let div = 2;
+    for(; div < num; div++) {
+        if(num % div === 0) {
+            console.log('Minimum divisor of a number', div);
+            break;
+        }
+    }
+    if(div === num) {
+        console.log('The number is prime:', num);
+    }
+}
