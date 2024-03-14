8f1b5c9d7ca42fff6240064f76fcf89ac8bdf387
Dmitrii Holubtsov <dmitrii.h@ideasoft.io>
2024-03-14 21:55:49 +0200

    hw_27 - GenerateKey function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const length = '16';
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+const generationResult = function generateKey(length, characters) {
+    let calcResult = '';
+    for (let i = 0; i < length; i++) {
+        calcResult += characters.charAt(Math.floor(Math.random() * length));
+    }
+    return calcResult;
+}
+
+const calcResult = generationResult(length, characters);
+console.log('Result: ', calcResult);
