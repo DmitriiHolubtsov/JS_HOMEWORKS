28d4667151d28a9df9f446587ddbf09c0d2918b7
Dmitrii Holubtsov <dmitrii.h@ideasoft.io>
2024-04-14 22:21:23 +0300

    hw_52 - Find factorial function

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-### HOMEWORK #1
+### HOMEWORK #52
+
+# Find factorial function
--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-"use strict";
+// Task 1
+const getFactorial = (num) => {
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    return num * getFactorial(num - 1);
+  }
+};
+console.log(getFactorial(3));
+
+// Task 2
+const numToPower = (num, degree) => {
+  if (degree === 1) {
+    return num;
+  } else if (degree === 0) {
+    return num;
+  } else if (degree > 1) {
+    return num * numToPower(num, degree - 1);
+  } else if (degree < 0) {
+    return 1 / numToPower(num, -degree);
+  }
+};
+console.log(numToPower(5, 2));
+
+// Task 3
+const mathNum = (a, b) => {
+  if (b === 0) {
+    return a;
+  } else {
+    return mathNum(++a, --b);
+  }
+};
+console.log(mathNum(10, 3));
