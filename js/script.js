23c9927dc1a447056b7ef69b94bbd5c9a02e7369
Dmitrii Holubtsov <dmitrii.h@ideasoft.io>
2024-04-17 11:25:05 +0300

    hw_52 - Bug fixing.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,27 @@
 // Task 1
 const getFactorial = (num) => {
-  if (num === 0 || num === 1) {
-    return 1;
-  } else {
-    return num * getFactorial(num - 1);
-  }
+  return num === 0 || num === 1 ? 1 : num * getFactorial(num - 1);
 };
 console.log(getFactorial(3));
 
 // Task 2
 const numToPower = (num, degree) => {
-  if (degree === 1) {
-    return num;
-  } else if (degree === 0) {
-    return num;
-  } else if (degree > 1) {
-    return num * numToPower(num, degree - 1);
-  } else if (degree < 0) {
-    return 1 / numToPower(num, -degree);
+  switch (true) {
+    case degree === 1:
+    case degree === 0:
+      return num;
+    case degree > 1:
+      return num * numToPower(num, degree - 1);
+    case degree < 0:
+      return 1 / numToPower(num, -degree);
+    default:
+      return NaN;
   }
 };
 console.log(numToPower(5, 2));
 
 // Task 3
 const mathNum = (a, b) => {
-  if (b === 0) {
-    return a;
-  } else {
-    return mathNum(++a, --b);
-  }
+  return b === 0 ? a : mathNum(++a, --b);
 };
 console.log(mathNum(10, 3));
