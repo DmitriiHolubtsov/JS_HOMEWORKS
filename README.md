a22abe9e5be9935f45ff8cff751a3bb24e35dc95
DmitriiHolubtsov <dmitriiholubtsov@gmail.com>
2024-05-27 16:05:41 +0300

    hw_66 - Storages V2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-### HOMEWORK #1
+### HOMEWORK #66
+
+# Storages V2 - Favorite Items list
--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-"use strict";
+(function () {
+  const favorites = JSON.parse(localStorage.getItem('favorites')) || {};
+
+  document.querySelectorAll('.item').forEach((item) => {
+    const itemId = item.dataset.id;
+    const button = item.querySelector('.favoriteBtn');
+    if (favorites[itemId]) {
+      item.classList.add('favorite');
+      button.textContent = 'Remove from favorites';
+    } else {
+      item.classList.remove('favorite');
+      button.textContent = 'Add to favorites';
+    }
+    button.addEventListener('click', () => {
+      if (favorites[itemId]) {
+        delete favorites[itemId];
+        item.classList.remove('favorite');
+        button.textContent = 'Add to favorites';
+      } else {
+        favorites[itemId] = true;
+        item.classList.add('favorite');
+        button.textContent = 'Remove from favorites';
+      }
+      localStorage.setItem('favorites', JSON.stringify(favorites));
+    });
+  });
+}());
