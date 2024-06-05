9c191735bf0da318eff176ea82575e47932d8c1c
DmitriiHolubtsov <dmitriiholubtsov@gmail.com>
2024-06-05 14:38:12 +0300

    hw_68 - Iterators

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-"use strict";
+const createArrayIterator = (function () {
+  return function (arr) {
+    let index = 0;
+
+    return {
+      next() {
+        if (index < arr.length) {
+          return { value: arr[index++], done: false };
+        }
+        return { value: undefined, done: true };
+      },
+    };
+  };
+}());
+
+const arr = [1, 2, 3, 4, 5];
+
+arr[Symbol.iterator] = function () {
+  return createArrayIterator(arr);
+};
+
+const iterator = arr[Symbol.iterator]();
+
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
